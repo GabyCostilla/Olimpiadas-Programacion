44f6531b770b9a0d2638740f296d65d23114f06e
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-03 19:12:05 -0300

    gpts arreglan el code de marcos

--- Desafio 0- Codificacion De Mensajes/c++.cpp
+++ Desafio 0- Codificacion De Mensajes/c++.cpp
@@ -1,3 +1,5 @@
+/*
+    VERSION ORIGINAL DE MARCOS PICCOTTO
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -17,10 +19,47 @@ int main() {
         if (i != ' ') {
             int pos = abc.find(i);
             if (k_aux == N) {
-               k_aux = 0;
+                k_aux = 0;
             }
             res += abc[(pos + key[k_aux]) % abc.size()];
             k_aux++;
+        }
+    }
+
+    cout << res << '\n';
+    return 0;
+}
+*/
+
+ //CHAT GPT VERSION CORREGIDA
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define N 2
+
+int main() {
+    string abc = "abcdefghijklmnñopqrstuvwxyz";
+    string word = "yo he logrado encender una cerilla";
+    string res = "";
+    int key[N] = {2, 3};
+    int k_aux = 0;
+
+    for (auto i : word) {
+        if (i != ' ') {
+            int pos = abc.find(i);
+            if (pos != string::npos) { // Verificar si la letra está en el alfabeto
+                if (k_aux == N) {
+                    k_aux = 0;
+                }
+                res += abc[(pos + key[k_aux]) % abc.size()];
+                k_aux++;
+            } else {
+                res += i; // Si no es una letra del alfabeto, se mantiene tal cual.
+            }
+        } else {
+            res += ' '; // Mantener los espacios en el mensaje cifrado
         }
     }
 
