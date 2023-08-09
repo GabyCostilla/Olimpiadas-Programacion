c679af9ec07ad98ecfa0257ebfa84ba2b291a1a1
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-09 18:39:58 -0300

    funciona a medias XD

--- Desafio 1/index.js
+++ Desafio 1/index.js
@@ -1,8 +1,29 @@
-function cadenas(a) {
-    if(a == 1){
-        return 1;
-    } else {
-        return 
+function calcularDivisores(numero) {
+  const divisores = [];
+  for (let i = 1; i <= numero; i++) {
+    if (numero % i === 0) {
+      divisores.push(i);
     }
+  }
+  return divisores;
 }
 
+function maximaSumaCadena(a) {
+  const divisores = calcularDivisores(a);
+  const dp = new Array(a + 1).fill(0);
+
+  for (let i = 1; i <= a; i++) {
+    dp[i] = i;
+    for (const divisor of divisores) {
+      if (i - divisor > 0) {
+        dp[i] = Math.max(dp[i], dp[i - divisor] + divisor);
+      }
+    }
+  }
+
+  return dp[a];
+}
+
+const numeroInicial = 60;
+const resultado = maximaSumaCadena(numeroInicial);
+console.log(`La máxima suma posible es: ${resultado}`);
