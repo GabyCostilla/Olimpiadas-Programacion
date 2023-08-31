90161784da0d227f8a7a4d10a3f97fbbaf6e9e06
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-31 11:24:16 -0300

    resolvemos ejercicio de recursividad

--- binary search/search.js
+++ binary search/search.js
@@ -1,11 +1,29 @@
-const arr = [40, 1, 5, 200];
+let array = [40, 1, 5, 200];
 function comparar(a, b) {
   return a - b;
 }
-arr.sort(comparar)
+array.sort(comparar)
 
-console.log(arr)
+console.log(array)
+    function buscarNumeroEnArray(array, numero, indice = 0) {
+        
+        if (indice === array.length) {
+            return false;
+        }
+        
+        if (array[indice] === numero) {
+            return true;
+        }
 
-function BinarySearch() {
+        return buscarNumeroEnArray(array, numero, indice + 1);
+    }
     
-}
+   
+    let numero = 3;
+    let resultado = buscarNumeroEnArray(array, numero);
+    if(resultado == true){
+        console.log(true)
+    } else{
+        console.log(false)
+    }
+    
